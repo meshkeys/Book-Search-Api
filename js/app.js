de01d8424ab7ef42753ca6bffe86e8e34a3e24ee
meshack <meshkeys@gmail.com>
2019-11-11 17:41:47 +0100

    book searc

--- js/app.js
+++ js/app.js
@@ -1,22 +1,31 @@
 function bookSearch(){
   var search = document.getElementById('Search').value
   document.getElementById('results').innerHTML = ""
-  console.log(search)
+
   $.ajax({
       url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
       dataType: "json",
-      success: function(data) {
-        console.log(data)
-          for (i = 0; i < data.items.length; i++){
-             title=results.innerHTML += `<h5 class="card border-success mb-3" style="max-width: 50rem;">` + data.items[i].volumeInfo.title + '</h5>'
-            aurthors=results.innerHTML += `<h5>` + data.items[i].volumeInfo.authors +`</h5>`
-              publisheDate=results.innerHTML += `<h5>` + data.items[i].volumeInfo.publishedDate +`</h5>`
-              description=results.innerHTML += `<h5 class="card" style="width: 50rem">` + data.items[i].volumeInfo.description +`</h5>`
-              
   
-          }
-      }, 
+      success: function(response) {
+          for (i = 0; i < response.items.length; i++){
+            title=$('<h5>' + response.items[i].volumeInfo.title + '</h5>');
+            img = $('<img class="">`<br><a href="' + response.items[i].volumeInfo.infoLink + '"><button id="imagebutton" class="btn red aligning">Read More</button></a>');
+            results.innerHTML += `<h5>` + response.items[i].volumeInfo.publisher + `</h5>`
+            results.innerHTML += `<h5>` + response.items[i].volumeInfo.publishedDate +`</h5>`
+            
+            if(response.items[i].volumeInfo == undefined){
+              } 
+              url= response.items[i].volumeInfo.imageLinks.thumbnail;
+              title.appendTo("#results");
+              img.attr('src', url);
+              img.appendTo("#results");
+
+            if(response.items[i].volumeInfo != undefined){
+
+            }
+        }
+  },
       type: 'GET'
   });
-} 
-document.getElementById('searchbtn').addEventListener('click', bookSearch, false); 
+}
+  document.getElementById('searchbtn').addEventListener('click', bookSearch, false);
